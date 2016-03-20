0a95d9fc79b3de6a7c9c3ab0e5bb17ff496c13ea
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-20 18:11:15 -0400

    add players to studio game

--- ruby/studio_game/studio_game.rb
+++ ruby/studio_game/studio_game.rb
@@ -1,8 +1,18 @@
-larry = 'larry'
-larry_health = 60
-curly = 'curly'
-moe = 'moe'
+current_time = Time.new.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "Welcome to the game! We're starting on #{current_time}: "
 
-puts "#{larry}'s health is #{larry_health}."
+larry = 'larry'
+larry_health = rand(61)
+curly = 'curly'
+curly_health = rand(126)
+moe = 'moe'
+moe_health = rand(101)
+shemp = 'shemp'
+shemp_health = rand(91)
 
 puts "Players: \n\t#{larry}\n\t#{curly}\n\t#{moe}"
+
+puts "#{larry} has a health of #{larry_health}."
+puts "#{curly.upcase} has a health of #{curly_health}."
+puts "#{moe.capitalize.center(13, '*')} has a health of #{moe_health}."
+puts "#{shemp.capitalize.ljust(28, '*')} #{shemp_health} health"
