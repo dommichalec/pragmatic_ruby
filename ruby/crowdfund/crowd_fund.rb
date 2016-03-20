f48e87629dddcb466172052abf9a471688279801
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-20 16:48:24 -0400

    add crowdfund and update studio game

---
+++ ruby/crowdfund/crowd_fund.rb
@@ -0,0 +1,6 @@
+project1 = 'ACME Fund'
+project1_funding = '$1000'
+project2 = 'Whitelabel Fund'
+project3 = 'AAA Fund'
+
+puts "#{project1}'s total funding is #{project1_funding}."
--- ruby/studio_game/studio_game.rb
+++ ruby/studio_game/studio_game.rb
@@ -1,4 +1,8 @@
 larry = 'larry'
 larry_health = 60
+curly = 'curly'
+moe = 'moe'
 
-puts "#{larry}'s health is #{larry_health.to_f / 9}."
+puts "#{larry}'s health is #{larry_health}."
+
+puts "Players: \n\t#{larry}\n\t#{curly}\n\t#{moe}"
