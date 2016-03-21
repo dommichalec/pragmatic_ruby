7685a5872b7e85717bab32a181068adced29f523
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-21 11:24:18 -0400

    clean up with methods

--- ruby/studio_game/studio_game.rb
+++ ruby/studio_game/studio_game.rb
@@ -1,18 +1,13 @@
-current_time = Time.new.strftime("%A %m/%d/%Y at %I:%M%p")
-puts "Welcome to the game! We're starting on #{current_time}: "
+def current_time
+  Time.new.strftime('%I:%M %p')
+end
 
-larry = 'larry'
-larry_health = rand(61)
-curly = 'curly'
-curly_health = rand(126)
-moe = 'moe'
-moe_health = rand(101)
-shemp = 'shemp'
-shemp_health = rand(91)
+def say_hello(name, health_score = 100)
+  "I'm #{name.capitalize} with a health of #{health_score} as of " \
+  "#{current_time}."
+end
 
-puts "Players: \n\t#{larry}\n\t#{curly}\n\t#{moe}"
-
-puts "#{larry} has a health of #{larry_health}."
-puts "#{curly.upcase} has a health of #{curly_health}."
-puts "#{moe.capitalize.center(13, '*')} has a health of #{moe_health}."
-puts "#{shemp.capitalize.ljust(28, '*')} #{shemp_health} health"
+puts say_hello('larry', 60)
+puts say_hello('curly', 125)
+puts say_hello('moe')
+puts say_hello('shemp', 90)
