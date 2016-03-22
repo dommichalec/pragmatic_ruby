970f0fa589a97b5ed4f18486ce3ab321f0945210
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-22 10:37:02 -0400

    fun with attr_* methods

--- ruby/studio_game/studio_game.rb
+++ ruby/studio_game/studio_game.rb
@@ -1,11 +1,11 @@
 class TypeError < StandardError; end
 
-def current_time
-  Time.new.strftime('%I:%M %p')
-end
-
 # Player class
 class Player
+  attr_accessor :name
+  attr_reader :health_score
+
+  # constructor
   def initialize(name, health_score = 100)
     fail TypeError unless name.is_a? String
     fail TypeError unless health_score.is_a? Integer
@@ -13,18 +13,29 @@ class Player
     @health_score = health_score
   end
 
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
+  def total_score
+    @health_score + @name.size
+  end
+
+  # mutates the health_score down by a random number between 1 and 10
   def blam!
     @health_score -= rand(11)
     puts "#{@name} was blammed! Health score has decreased to #{@health_score}."
   end
 
+  # mutates the health_score up by a random number between 1 and 15
   def w00t!
     @health_score += rand(16)
     puts "#{@name} was w00ted! Health score has increased to #{@health_score}."
   end
 
+  # practice with the Time class
   def to_s
-    "#{@name} has a health of #{@health_score} as of today at" \
+    "#{@name} has a total score of #{total_score} as of today at" \
     " #{Time.new.strftime('%I:%M %p')}"
   end
 end
@@ -33,9 +44,6 @@ larry = Player.new('larry', 60)
 curly = Player.new('curly', 125)
 moe = Player.new('moe')
 shemp = Player.new('shemp', 90)
-puts larry
-puts curly
-puts moe
-puts shemp
 
-curly.w00t!
+larry.name = 'Lawrence'
+puts larry
